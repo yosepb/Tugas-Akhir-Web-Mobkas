18c56b7c7738e419ac448bb01217220244f29dab
Yosep Bahtiar <yosep123tiar@gmail.com>
2023-10-18 08:00:01 +0700

    Perubahan #3, mengubah file-file inti dan menambah halaman PageDetailCar

---
+++ src/pages/PageDetailCar.jsx
@@ -0,0 +1,10 @@
+const PageDetailCar = () => {
+
+    return (
+        <>
+            <h1>Halaman Detail Kendaraan</h1>
+        </>
+    )
+}
+
+export default PageDetailCar;
---
+++ src/pages/PageLayout.jsx
@@ -0,0 +1,10 @@
+import { Outlet } from "react-router-dom";
+
+export default function PageLayout() {
+    return (
+        <>
+            <h1>Halaman Layout "/"</h1>
+            <Outlet/>
+        </>
+    );
+}

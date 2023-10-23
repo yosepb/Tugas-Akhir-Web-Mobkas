83cb0880157160dcedabbce1e01d2a74a90a02ca
gilangerlangga18 <gilang.erlangga1806@gmail.com>
2023-10-23 13:01:50 +0700

    style button list all car

--- src/pages/MainPages.jsx
+++ src/pages/MainPages.jsx
@@ -4,6 +4,7 @@ import Navbar from "../Component/Navbar";
 import MainBanner from "../Component/MainBanner";
 import TutorialBuyCar from "../Component/TutorialBuyCar";
 import ListMobil from "../Component/ListMobil";
+import { BiSolidDownArrow } from "react-icons/Bi";
 
 function MainPages() {
   const [toggle, setToggle] = useState(false);
@@ -38,27 +39,45 @@ function MainPages() {
           alignItems: "center",
           width: "100%",
           backgroundColor: "#121b6e",
+          position: "relative",
         }}
       >
         <CarouselCar />
+        <button
+          style={{
+            fontSize: "20px",
+            fontWeight: "600",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            border: "none",
+            marginTop: "80px",
+            textAlign: "center",
+            flexDirection: "column",
+            backgroundColor: "transparent",
+            position: "absolute",
+            right: "300px",
+          }}
+          onClick={() => setToggle(!toggle)}
+        >
+          <p
+            style={{
+              color: "white",
+            }}
+          >
+            All Car
+          </p>
+          <BiSolidDownArrow
+            style={{
+              width: "150px",
+              height: "150px",
+              marginTop: "-40px",
+              color: "yellow",
+            }}
+          />
+        </button>
       </div>
-      <button
-        style={{
-          width: "120px",
-          height: "40px",
-          fontSize: "20px",
-          fontWeight: "600",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: "yellow",
-          border: "none",
-          marginTop: "80px",
-        }}
-        onClick={() => setToggle(!toggle)}
-      >
-        <p>All Car</p>
-      </button>
+
       {toggle && (
         <div>
           <p
@@ -68,6 +87,7 @@ function MainPages() {
               display: "flex",
               alignSelf: "center",
               justifyContent: "center",
+              textAlign: "center",
             }}
           >
             {" "}
